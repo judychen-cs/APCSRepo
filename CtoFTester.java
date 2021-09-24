5115fa9f738180a1d5a8cf8c01f722b57a756bcb
Judy Chen <jchen33@stuy.edu>
2021-09-23 23:12:14 -0400

    modifications

--- CtoFTester.java
+++ CtoFTester.java
@@ -5,15 +5,15 @@
 
 public class CtoFTester {
   public static double celsiusToFahrenheit(double C) {
-    double Fahrenheit = ((9.0/5.0) * C) + 32;
+    double Fahrenheit = ((9.0/5.0) * C) + 32.0;
     return Fahrenheit;
   }
   public static double fahrenheittoCelsius(double F) {
-    double Celsius = (5.0/9.0) * (F - 32);
+    double Celsius = (5.0/9.0) * (F - 32.0);
     return Celsius;
   }
-  public static double main(String[] args){
-    System.out.print("Celsius of 0 degrees F" + fahrenheittoCelsius(0));
-    System.out.print("Fahrenheit of 0 degrees C" + celsiusToFahrenheit(0));
+  public static void main(String[] args){
+    System.out.print("Celsius of 0 degrees F" + " = " + fahrenheittoCelsius(0));
+    System.out.print("Fahrenheit of 0 degrees C" + " = " + celsiusToFahrenheit(0));
   }
 }
