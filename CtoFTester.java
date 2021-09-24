6d97b4e3be7a81bb3c90a9e72a3cab15dd4cf749
Judy C <78173119+judychen-cs@users.noreply.github.com>
2021-09-23 22:05:18 -0400

    Update CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,4 +2,13 @@
 // The first function should take Celsius as a parameter, while the other should have Fahrenheit. 
 // The first function should return a double type value Fahrenheit, while the other should return a double type value Celsius. 
 
-
+public class conversions {
+  public static double celsiusToFahrenheit(double C) {
+    double Fahrenheit = ((9.0/5.0) * C) + 32;
+    return Fahrenheit;
+  }
+  public static double fahrenheittoCelsius(double F) {
+    double Celsius = (5.0/9.0) * (F - 32);
+    return Celsius;
+  }
+}
