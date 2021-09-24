4a3380b0cb983bdf807b41ccc70365a9659c6631
Judy C <78173119+judychen-cs@users.noreply.github.com>
2021-09-23 22:21:15 -0400

    Update CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,4 @@
+// Hello! 
 // For the parameters, the functions should have one double type each. 
 // The first function should take Celsius as a parameter, while the other should have Fahrenheit. 
 // The first function should return a double type value Fahrenheit, while the other should return a double type value Celsius. 
